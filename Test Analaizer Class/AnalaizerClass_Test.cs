0a47188a9ece449066a33d711b84fbcabca37fb2
Дмитрий <@DESKTOP-B26T9UQ>
2019-02-27 23:44:27 +0400

    Add project files.

---
+++ Test Analaizer Class/AnalaizerClass_Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseCalculator;
+
+namespace Test_Analaizer_Class
+{
+    class AnalaizerClass_Test
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void CheckCurrencyTest()
+        {
+            AnalaizerClass.expression = Console.ReadLine();
+            bool result = AnalaizerClass.CheckCurrency();
+            Console.WriteLine(result);
+        }
+
+        static void FormatTest()
+        {
+            AnalaizerClass.expression = Console.ReadLine();
+            string result = AnalaizerClass.Format();
+            Console.WriteLine(result);
+        }
+
+        static void CreateStackTest()
+        {
+            AnalaizerClass.expression = Console.ReadLine();
+            var result = AnalaizerClass.CreateStack();
+            foreach(var temp in result)
+            {
+                Console.WriteLine(temp);
+            }
+        }
+
+        static void RunEstimateTest()
+        {
+            AnalaizerClass.expression = Console.ReadLine();
+            AnalaizerClass.RunEstimate();
+            Console.WriteLine(AnalaizerClass.expression);
+        }
+    }
+}
---
+++ Test Calc Class/CalcClass_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseCalculator;
+
+
+namespace Testing_tasks
+{
+    class CalcClass_Test
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            long a = ran.Next(int.MaxValue);
+            long b = ran.Next(int.MaxValue);
+            for (int i = 0; i < 3; i++)
+            {
+                long result = CalcClass.Add(a, b);
+                if (result == a + b)
+                {
+                    Console.WriteLine("a = {0}, b = {1}, a + b = {2}", a, b, a + b);
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("a = {0}, b = {1}, a + b = {2}", a, b, a + b);
+                    Console.WriteLine("False");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
